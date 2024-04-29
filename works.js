25b08062bc5a18848ca8a7541b2c0cc6406b807f
Anna <52211360+AnnaMTZ@users.noreply.github.com>
2024-04-29 18:52:59 +0200

    wip: gallery in progress

--- works.js
+++ works.js
@@ -1,84 +1,103 @@
-const images = document.querySelectorAll('.galleryImage');
+let images = document.querySelectorAll(".galleryImage");
 let windowWidth = window.innerWidth;
 
+let currentGallery = "";
+let getLatestOpenedImg;
+
 if (images) {
-	images.forEach((img, index) => {
-		img.addEventListener('click', () => {
-			// let getElementCss = window.getComputedStyle(img);
-			// let getFullImgUrl = getElementCss.getPropertyValue('background-image');
+  images.forEach((img, index) => {
+    img.addEventListener("click", () => {
+      // let getElementCss = window.getComputedStyle(img);
+      // let getFullImgUrl = getElementCss.getPropertyValue('background-image');
 
-			// let getImgUrlPos = getFullImgUrl.split('/photo/bor');
-			// console.log(getFullImgUrl);
+      // let getImgUrlPos = getFullImgUrl.split('/photo/bor');
+      // console.log(getFullImgUrl);
 
-			// let setNewImgUrl = getImgUrlPos[1].replace('")', '');
+      // let setNewImgUrl = getImgUrlPos[1].replace('")', '');
 
-			getLatestOpenedImg = index + 1;
+      imgFolder = ["bor", "can"];
 
-			let container = document.body;
-			let newImgWindow = document.createElement('div');
-			newImgWindow.setAttribute('class', 'img-window');
-			newImgWindow.setAttribute('onclick', 'closeImg()');
-			container.appendChild(newImgWindow);
-			console.log('it works');
+      imgFolder.forEach((folderName) => {
+        if (img.childNodes[1].src.includes(folderName)) {
+          images = `images${folderName}`;
+          folder = folderName;
+          currentGallery = document.querySelectorAll(`.galleryImage${folder}`);
+          //   currentGallery.forEach(_, (index) => {
+          //     console.log(index);
+          //   });
+        } else {
+          console.log(false);
+        }
+      });
 
-			let newImg = document.createElement('img');
-			newImgWindow.appendChild(newImg);
-			newImg.setAttribute('src', `/photo/bor/bor${getLatestOpenedImg}.jpg`);
-			newImg.setAttribute('id', 'current-img');
+      currentGallery.forEach((gallery, index) => {
+        return (getLatestOpenedImg = index + 1);
+      });
 
-			//the code below will run after the image is loaded
-			newImg.onload = function () {
-				let imgWidth = this.width;
-				let calcImgToEdge = (windowWidth - imgWidth) / 2 - 80;
-				console.log(calcImgToEdge);
+      let container = document.body;
+      let newImgWindow = document.createElement("div");
+      newImgWindow.setAttribute("class", "img-window");
+      newImgWindow.setAttribute("onclick", "closeImg()");
+      container.appendChild(newImgWindow);
 
-				let newPrevBtn = document.createElement('a');
-				let btnPrevText = document.createTextNode('prev');
-				newPrevBtn.appendChild(btnPrevText);
-				container.appendChild(newPrevBtn);
-				newPrevBtn.setAttribute('class', 'prev-btn');
-				newPrevBtn.setAttribute('onclick', 'changeImg(0)');
-				// newPrevBtn.style.cssText = 'left: ' + calcImgToEdge + 'px;'; // to fix
+      let newImg = document.createElement("img");
+      newImgWindow.appendChild(newImg);
+      newImg.setAttribute("src", `/photo/${folder}/${folder}${getLatestOpenedImg}.jpg`);
+      newImg.setAttribute("id", "current-img");
 
-				let newNextBtn = document.createElement('a');
-				let btnNextText = document.createTextNode('next');
-				newNextBtn.appendChild(btnNextText);
-				container.appendChild(newNextBtn);
-				newNextBtn.setAttribute('class', 'next-btn');
-				newNextBtn.setAttribute('onclick', 'changeImg(1)');
-				// newNextBtn.style.cssText = 'left: ' + calcImgToEdge + 'px;'; // to fix
-			};
-		});
-	});
+      //the code below will run after the image is loaded
+      newImg.onload = function () {
+        let imgWidth = this.width;
+        let calcImgToEdge = (windowWidth - imgWidth) / 2 - 80;
+        console.log(calcImgToEdge);
+
+        let newPrevBtn = document.createElement("a");
+        let btnPrevText = document.createTextNode("prev");
+        newPrevBtn.appendChild(btnPrevText);
+        container.appendChild(newPrevBtn);
+        newPrevBtn.setAttribute("class", "prev-btn");
+        newPrevBtn.setAttribute("onclick", "changeImg(0)");
+        // newPrevBtn.style.cssText = 'left: ' + calcImgToEdge + 'px;'; // to fix
+
+        let newNextBtn = document.createElement("a");
+        let btnNextText = document.createTextNode("next");
+        newNextBtn.appendChild(btnNextText);
+        container.appendChild(newNextBtn);
+        newNextBtn.setAttribute("class", "next-btn");
+        newNextBtn.setAttribute("onclick", "changeImg(1)");
+        // newNextBtn.style.cssText = 'left: ' + calcImgToEdge + 'px;'; // to fix
+      };
+    });
+  });
 }
 
 function closeImg() {
-	document.querySelector('.img-window').remove();
-	document.querySelector('.prev-btn').remove();
-	document.querySelector('.next-btn').remove();
+  document.querySelector(".img-window").remove();
+  document.querySelector(".prev-btn").remove();
+  document.querySelector(".next-btn").remove();
 }
 
 function changeImg(changeDir) {
-	document.querySelector('#current-img').remove();
-	let getImgWindow = document.querySelector('.img-window');
-	let newImg = document.createElement('img');
-	getImgWindow.appendChild(newImg);
+  document.querySelector("#current-img").remove();
+  let getImgWindow = document.querySelector(".img-window");
+  let newImg = document.createElement("img");
+  getImgWindow.appendChild(newImg);
 
-	let calcNewImg;
+  let calcNewImg;
 
-	if (changeDir === 1) {
-		calcNewImg = getLatestOpenedImg + 1;
-		if (calcNewImg > images.length) {
-			calcNewImg = 1;
-		}
-	} else if (changeDir === 0) {
-		calcNewImg = getLatestOpenedImg - 1;
-		if (calcNewImg < 1) {
-			calcNewImg = images.length;
-		}
-	}
+  if (changeDir === 1) {
+    calcNewImg = getLatestOpenedImg + 1;
+    if (calcNewImg > images.length) {
+      calcNewImg = 1;
+    }
+  } else if (changeDir === 0) {
+    calcNewImg = getLatestOpenedImg - 1;
+    if (calcNewImg < 1) {
+      calcNewImg = images.length;
+    }
+  }
 
-	newImg.setAttribute('src', `/photo/bor/bor${calcNewImg}.jpg`);
-	newImg.setAttribute('id', 'current-img');
-	getLatestOpenedImg = calcNewImg;
+  newImg.setAttribute("src", `/photo/${folder}/${folder}${calcNewImg}.jpg`);
+  newImg.setAttribute("id", "current-img");
+  getLatestOpenedImg = calcNewImg;
 }
