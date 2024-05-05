8388549eb5d4ff87f91ec484853c4c70bec682c5
Anna <52211360+AnnaMTZ@users.noreply.github.com>
2024-05-05 22:06:36 +0200

    wip: added galleries to html

--- works.js
+++ works.js
@@ -1,40 +1,15 @@
-let images = document.querySelectorAll(".galleryImage");
+// GENERAL //
 let windowWidth = window.innerWidth;
 
-let currentGallery = "";
-let getLatestOpenedImg;
+/// BORYSPIL ///
 
-if (images) {
-  images.forEach((img, index) => {
+let imagesBor = document.querySelectorAll(".galleryImageBor");
+if (imagesBor) {
+  imagesBor.forEach((img, index) => {
     img.addEventListener("click", () => {
-      // let getElementCss = window.getComputedStyle(img);
-      // let getFullImgUrl = getElementCss.getPropertyValue('background-image');
+      getLatestOpenedImg = index + 1;
 
-      // let getImgUrlPos = getFullImgUrl.split('/photo/bor');
-      // console.log(getFullImgUrl);
-
-      // let setNewImgUrl = getImgUrlPos[1].replace('")', '');
-
-      imgFolder = ["bor", "can"];
-
-      imgFolder.forEach((folderName) => {
-        if (img.childNodes[1].src.includes(folderName)) {
-          images = `images${folderName}`;
-          folder = folderName;
-          currentGallery = document.querySelectorAll(`.galleryImage${folder}`);
-          //   currentGallery.forEach(_, (index) => {
-          //     console.log(index);
-          //   });
-        } else {
-          console.log(false);
-        }
-      });
-
-      currentGallery.forEach((gallery, index) => {
-        return (getLatestOpenedImg = index + 1);
-      });
-
-      let container = document.body;
+      let container = document.querySelector(".boryspil");
       let newImgWindow = document.createElement("div");
       newImgWindow.setAttribute("class", "img-window");
       newImgWindow.setAttribute("onclick", "closeImg()");
@@ -42,14 +17,13 @@ if (images) {
 
       let newImg = document.createElement("img");
       newImgWindow.appendChild(newImg);
-      newImg.setAttribute("src", `/photo/${folder}/${folder}${getLatestOpenedImg}.jpg`);
+      newImg.setAttribute("src", `/photo/bor/bor${getLatestOpenedImg}.jpg`);
       newImg.setAttribute("id", "current-img");
 
       //the code below will run after the image is loaded
       newImg.onload = function () {
         let imgWidth = this.width;
         let calcImgToEdge = (windowWidth - imgWidth) / 2 - 80;
-        console.log(calcImgToEdge);
 
         let newPrevBtn = document.createElement("a");
         let btnPrevText = document.createTextNode("prev");
@@ -57,7 +31,7 @@ if (images) {
         container.appendChild(newPrevBtn);
         newPrevBtn.setAttribute("class", "prev-btn");
         newPrevBtn.setAttribute("onclick", "changeImg(0)");
-        // newPrevBtn.style.cssText = 'left: ' + calcImgToEdge + 'px;'; // to fix
+        // newPrevBtn.style.cssText = "right: " + calcImgToEdge + "px;"; // to fix
 
         let newNextBtn = document.createElement("a");
         let btnNextText = document.createTextNode("next");
@@ -65,7 +39,7 @@ if (images) {
         container.appendChild(newNextBtn);
         newNextBtn.setAttribute("class", "next-btn");
         newNextBtn.setAttribute("onclick", "changeImg(1)");
-        // newNextBtn.style.cssText = 'left: ' + calcImgToEdge + 'px;'; // to fix
+        // newNextBtn.style.cssText = "left: " + calcImgToEdge + "px;"; // to fix
       };
     });
   });
@@ -87,17 +61,94 @@ function changeImg(changeDir) {
 
   if (changeDir === 1) {
     calcNewImg = getLatestOpenedImg + 1;
-    if (calcNewImg > images.length) {
+    if (calcNewImg > imagesBor.length) {
       calcNewImg = 1;
     }
   } else if (changeDir === 0) {
     calcNewImg = getLatestOpenedImg - 1;
     if (calcNewImg < 1) {
-      calcNewImg = images.length;
+      calcNewImg = imagesBor.length;
     }
   }
 
-  newImg.setAttribute("src", `/photo/${folder}/${folder}${calcNewImg}.jpg`);
+  newImg.setAttribute("src", `/photo/bor/bor${calcNewImg}.jpg`);
   newImg.setAttribute("id", "current-img");
   getLatestOpenedImg = calcNewImg;
 }
+
+/// CANADIAN VILLAGE ///
+
+let imagesCan = document.querySelectorAll(".galleryImageCan");
+if (imagesCan) {
+  imagesCan.forEach((img, index) => {
+    img.addEventListener("click", () => {
+      getLatestOpenedImg = index + 1;
+
+      let container = document.querySelector(".canada");
+      let newImgWindow = document.createElement("div");
+      newImgWindow.setAttribute("class", "img-window");
+      newImgWindow.setAttribute("onclick", "closeImg()");
+      container.appendChild(newImgWindow);
+
+      let newImg = document.createElement("img");
+      newImgWindow.appendChild(newImg);
+      newImg.setAttribute("src", `/photo/can/can${getLatestOpenedImg}.jpg`);
+      newImg.setAttribute("id", "current-img");
+
+      //the code below will run after the image is loaded
+      newImg.onload = function () {
+        let imgWidth = this.width;
+        let calcImgToEdge = (windowWidth - imgWidth) / 2 - 80;
+
+        let newPrevBtn = document.createElement("a");
+        let btnPrevText = document.createTextNode("prev");
+        newPrevBtn.appendChild(btnPrevText);
+        container.appendChild(newPrevBtn);
+        newPrevBtn.setAttribute("class", "prev-btn");
+        newPrevBtn.setAttribute("onclick", "changeImg(0)");
+        // newPrevBtn.style.cssText = "right: " + calcImgToEdge + "px;"; // to fix
+
+        let newNextBtn = document.createElement("a");
+        let btnNextText = document.createTextNode("next");
+        newNextBtn.appendChild(btnNextText);
+        container.appendChild(newNextBtn);
+        newNextBtn.setAttribute("class", "next-btn");
+        newNextBtn.setAttribute("onclick", "changeImg(1)");
+        // newNextBtn.style.cssText = "left: " + calcImgToEdge + "px;"; // to fix
+      };
+    });
+  });
+}
+
+function closeImg() {
+  document.querySelector(".img-window").remove();
+  document.querySelector(".prev-btn").remove();
+  document.querySelector(".next-btn").remove();
+}
+
+function changeImg(changeDir) {
+  document.querySelector("#current-img").remove();
+  let getImgWindow = document.querySelector(".img-window");
+  let newImg = document.createElement("img");
+  getImgWindow.appendChild(newImg);
+
+  let calcNewImg;
+
+  if (changeDir === 1) {
+    calcNewImg = getLatestOpenedImg + 1;
+    if (calcNewImg > imagesCan.length) {
+      calcNewImg = 1;
+    }
+  } else if (changeDir === 0) {
+    calcNewImg = getLatestOpenedImg - 1;
+    if (calcNewImg < 1) {
+      calcNewImg = imagesCan.length;
+    }
+  }
+
+  newImg.setAttribute("src", `/photo/can/can${calcNewImg}.jpg`);
+  newImg.setAttribute("id", "current-img");
+  getLatestOpenedImg = calcNewImg;
+}
+
+/// GORENYCHI ///
